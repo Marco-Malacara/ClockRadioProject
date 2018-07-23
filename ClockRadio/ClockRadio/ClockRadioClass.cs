1a73cf3ec9f023cc4e47b38d7576168734a81cda
Marco-Malacara <marco.malacara7@gmail.com>
2018-07-23 10:48:35 -0500

    ability to change alarm time and turn it on or off.

--- ClockRadio/ClockRadio/ClockRadioClass.cs
+++ ClockRadio/ClockRadio/ClockRadioClass.cs
@@ -9,7 +9,8 @@ namespace ClockRadio
     public class ClockRadioClass
     {
         public DateTime currentTime; //Display Current Time X 
-        public double currentAlarmTime; // display current alarm time
+        public double currentAlarmTime; // display current alarm time x
+        public double setAlarm; // user ability to set alarm x
         public bool alarmOnOff; //turn alarm on and off
         public double changeRadioStation; // dipslay current radio station / change radio station
 
@@ -21,17 +22,23 @@ namespace ClockRadio
         }    
         public void CurrentAlarm()
         {
-            Console.WriteLine("Your current alarm is set for 11:00 A.M. Would you like to turn this alarm on/off? Press 1 for ON or Press 2 for OFF.");
+            Console.WriteLine("Your current alarm is set for 11:00 A.M. Would you like to change alarm time? Press 1 to change or Press 2 to keep current alarm time.");
             double.TryParse(Console.ReadLine(), out currentAlarmTime);
-            if(currentAlarmTime == 1)
+            if (currentAlarmTime == 1)
             {
-                Console.WriteLine("Your 11:00 A.M. alarm is now turned on!");
+                Console.WriteLine("What time would you like to set an alarm for?");
                 Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Your 11:00 A.M. alarm has been turned off!");
-                Console.ReadLine();
+                Console.WriteLine("Would you like to turn this alarm on? Press 1 for Yes or Press 2 for no.");
+                double.TryParse(Console.ReadLine(), out setAlarm);
+                if (setAlarm == 1)
+                {
+                    Console.WriteLine("Congrats! Your alarm has been turned on!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your 11:00A.M. alarm is been turned on!");
+                }
             }
         }
     }
